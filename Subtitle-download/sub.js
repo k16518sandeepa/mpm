f08858eb4d2e95aaa195d330c09dba8214f7edd9
Sandeepa Thennakoon <k16518.sandeepa@gmail.com>
2025-11-12 09:44:24 +0530

    bot offline

--- Subtitle-download/sub.js
+++ Subtitle-download/sub.js
@@ -422,7 +422,7 @@
       displaySubs(filtered);
     });
 
-    document.getElementById("bot-status").classList.add("");
+    document.getElementById("bot-status").classList.add("active");
 
 //ads sideshow
 const ads = [
